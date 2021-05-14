29662b373b02c0aedaf8cf46c08f9a0ebfaba87c
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-05-14 12:58:11 +0200

    improved default path

--- src/strawb/config_parser/config_parser.py
+++ src/strawb/config_parser/config_parser.py
@@ -6,20 +6,24 @@ class Config:
     config = configparser.ConfigParser()
     path = os.path.expanduser('~/.strawb/config')
 
-    # if this path doesn't exist, use the default from the repro
+    # go back 3 times: config_parser + strawb + src
+    repository_home = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
+    print(repository_home)
+    # if this path doesn't exist, use the default from the repository
     if not os.path.exists(path):
-        path = os.path.dirname(__file__)  # the config and definitions.py are in the same directory
-        path = os.path.abspath(os.path.join(path, '../../..'))  # go back 3 times: config_parser + strawb + src
-        path = os.path.join(path, 'config')
+        path = os.path.join(repository_home, 'config')
 
     config.read(path)
 
     for i in config['Paths']:
-        path = os.path.expanduser(config['Paths'][i])
-        path = os.path.abspath(path)
+        path = config['Paths'][i]
+        path = path.format(RepositoryHome=repository_home)
+        path = os.path.abspath(os.path.expanduser(path))
         config.set('Paths', i, path)
 
     raw_data_dir = config.get('Paths', 'raw_data_dir')
     proc_data_dir = config.get('Paths', 'proc_data_dir')
 
     token = config.get('ONC', 'token')
+
+print(Config.raw_data_dir)
