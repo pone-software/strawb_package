116055e092203962dd4453ab76256f70c7786c75
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-07-14 15:24:26 +0200

    added consistency check DatasetsInGroupSameSize

--- src/strawb/virtual_hdf5.py
+++ src/strawb/virtual_hdf5.py
@@ -2,13 +2,14 @@ import h5py
 
 
 class VirtualHDF5:
-    def __init__(self, file_name, file_name_list):
+    def __init__(self, file_name, file_name_list, obj_dict_filter=None):
         self.file_name = file_name
         self.file_name_list = file_name_list
+        self.obj_dict_filter = obj_dict_filter
 
-        obj_dict = self.get_obj_dict(file_name_list)
-        obj_dict = self.add_layout(obj_dict)
-        self.create_virtual_file(obj_dict, file_name_list)
+        self.obj_dict = self.get_obj_dict()
+        self.obj_dict = self.add_layout()
+        self.create_virtual_file()
 
     def __iter_group__(self, obj, obj_dict=None, file_index=0):
         if obj_dict is None:
@@ -54,18 +55,39 @@ class VirtualHDF5:
                 obj_dict[obj.name]['attrs'][key_i] = value_i
         return obj_dict
 
-    def get_obj_dict(self, file_name_list):
-        obj_dict = {}
-        for i, file_i in enumerate(file_name_list):
-            with h5py.File(file_i, 'r') as f:
-                # for i in f.items():
-                self.__iter_group__(f, obj_dict=obj_dict, file_index=i)
-        return obj_dict
+    @staticmethod
+    def groups_from_obj_dict(obj_dict):
+        return [i for i, item in obj_dict.items() if 'VDataSets' not in item]
 
     @staticmethod
-    def add_layout(obj_dict):
+    def datasets_from_obj_dict(obj_dict):
+        return [i for i, item in obj_dict.items() if 'VDataSets' in item]
+
+    def get_obj_dict(self, file_name_list=None):
+        if file_name_list is not None:  # file_name_list can be updated later
+            self.file_name_list = file_name_list
+
+        obj_dict = {}
+        for i, file_i in enumerate(self.file_name_list):
+            try:
+                with h5py.File(file_i, 'r', libver='latest', swmr=True) as f:
+                    # for i in f.items():
+                    self.__iter_group__(f, obj_dict=obj_dict, file_index=i)
+            except OSError as err:
+                print(f'Error at {file_i}: {err}')
+
+        if self.obj_dict_filter is not None:
+            obj_dict = self.obj_dict_filter.filter(obj_dict)
+
+        return obj_dict
+
+    def add_layout(self, obj_dict=None):
         """Adds the layout to each item of the obj_dict."""
-        datasets = {i: obj_dict[i] for i in obj_dict if 'VDataSets' in obj_dict[i]}  # get only the datasets
+        if obj_dict is not None:  # obj_dict can be updated later
+            self.obj_dict = obj_dict
+
+        # get only the datasets
+        datasets = {i: self.obj_dict[i] for i in self.obj_dict if 'VDataSets' in self.obj_dict[i]}
         for key_i, obj_i in datasets.items():
             dtype = obj_i['VDataSets'][0].dtype
             if dtype == object:
@@ -82,22 +104,99 @@ class VirtualHDF5:
                 layout_i[offset: offset + length] = v_source
                 offset += length
 
-            obj_dict[key_i].update({'layout': layout_i})
+            self.obj_dict[key_i].update({'layout': layout_i})
 
-        return obj_dict
+        return self.obj_dict
 
-    def create_virtual_file(self, obj_dict, file_name_list):
+    def create_virtual_file(self, file_name=None, obj_dict=None):
+        if file_name is not None:  # file name can be updated later
+            self.file_name = file_name
+
+        if obj_dict is not None:  # obj_dict can be updated later
+            self.obj_dict = obj_dict
+
         with h5py.File(self.file_name, 'w', libver='latest') as f:
-            f.attrs.update({'file_names': file_name_list})
+            f.attrs.update({'file_names': self.file_name_list})
 
             # create groups
-            groups = {i: obj_dict[i] for i in obj_dict if 'VDataSets' not in obj_dict[i]}  # get only the datasets
+            # get only the datasets
+            groups = {i: self.obj_dict[i] for i in self.obj_dict if 'VDataSets' not in self.obj_dict[i]}
             for key_i, obj_i in groups.items():
                 group = f.require_group(key_i)  # create the group in case, otherwise create dataset fails
                 group.attrs.update(obj_i['attrs'])
 
             # create datasets
-            datasets = {i: obj_dict[i] for i in obj_dict if 'VDataSets' in obj_dict[i]}  # get only the datasets
+            # get only the datasets
+            datasets = {i: self.obj_dict[i] for i in self.obj_dict if 'VDataSets' in self.obj_dict[i]}
             for key_i, obj_i in datasets.items():
                 dataset = f.create_virtual_dataset(key_i, obj_i['layout'], fillvalue=0)
                 dataset.attrs.update(obj_i['attrs'])
+
+
+class DatasetsInGroupSameSize:
+    """A Filter class which takes a obj_dict from VirtualHDF5 and checks if a group across all files have the same
+    datasets and if the datasets in that group have the same shape along the first axis inside each file.
+    In case it removes all datasets of the effected group for this file. In other words, a non consistent dataset in a
+    cause that all datasets of its group and for this file aren't added to the virtual HDF5 File. Other groups for the
+    same file or the group for other files aren't effected."""
+    @staticmethod
+    def filter(obj_dict):
+        hdf5_structure_dict = DatasetsInGroupSameSize.get_hdf5_structure(obj_dict)
+        invalid_dict = DatasetsInGroupSameSize.get_invalid_groups(obj_dict, hdf5_structure_dict)
+        obj_dict = DatasetsInGroupSameSize.pop_invalid_datasets(obj_dict, invalid_dict, hdf5_structure_dict)
+        return obj_dict
+
+    @staticmethod
+    def get_hdf5_structure(obj_dict):
+        datasets = [i for i, item in obj_dict.items() if 'VDataSets' in item]
+
+        hdf5_structure_dict = {}
+        for group, key in [[i.rsplit('/', 1)[0], i] for i in datasets]:
+            if group in hdf5_structure_dict:
+                hdf5_structure_dict[group].append(key)
+            else:
+                hdf5_structure_dict[group] = [key]
+        return hdf5_structure_dict
+
+    @staticmethod
+    def get_invalid_groups(obj_dict, hdf5_structure_dict):
+        invalid_dict = {}
+        for group_i in hdf5_structure_dict:
+            path_dict = {}
+
+            for key_i in hdf5_structure_dict[group_i]:
+                for j in obj_dict[key_i]['VDataSets']:
+                    if j.path in path_dict:
+                        path_dict[j.path].append(j.shape[0])
+                    else:
+                        path_dict[j.path] = [j.shape[0]]
+
+            invalid_pathes = set()
+            for key_i, value_i in path_dict.items():
+                if len(value_i) != len(hdf5_structure_dict[group_i]):
+                    invalid_pathes.update([key_i])
+                    print(f'Not all datasets in group: "{group_i}"; file: "{key_i}"')
+                elif len(set(value_i)) != 1:
+                    invalid_pathes.update([key_i])
+                    print(f'More than one shape in group: "{group_i}"; file: "{key_i}"; shapes: {value_i}')
+
+            if invalid_pathes:
+                invalid_dict[group_i] = invalid_pathes
+
+        return invalid_dict
+
+    @staticmethod
+    def pop_invalid_datasets(obj_dict, invalid_dict, hdf5_structure_dict):
+        for group_i, files_i in invalid_dict.items():
+            j_pop = []
+            for dataset_j in hdf5_structure_dict[group_i]:
+                j_pop = [j for j, set_j in enumerate(obj_dict[dataset_j]['VDataSets']) if set_j.path in files_i]
+
+            j_pop.sort()  # sort the indexes
+            for j in j_pop[::-1]:  # pop items from the end, otherwise indexes change
+                obj_j = obj_dict[dataset_j]['VDataSets'].pop(j)
+                obj_dict[dataset_j]['total_length'] -= obj_j.shape[0]
+                # print(dataset_j, obj_j.path, obj_j.shape[0])
+
+        return obj_dict
+
