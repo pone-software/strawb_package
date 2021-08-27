582f073571c1ab3d14ffdaf5a6fc05986e2ab0a6
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-08-27 15:42:10 +0200

    updated example to work with new functions from the fork

--- examples/onc_vm_downloader.py
+++ examples/onc_vm_downloader.py
@@ -1,81 +1,47 @@
 #!/usr/bin/python3
+# coding: utf-8
 
 # This examples shows who to download files from the ONC server
+
+import datetime
+import pandas as pd
 import strawb
 
-import os
-import pandas as pd
-import datetime
-from dateutil.relativedelta import relativedelta
-import numpy as np
-
-
-def download_module(dev_code):
-    """Download the files of interest for a module over the entire period of STRAW-b
-    by wrapping 'download_files_of_interest'.
-    PARAMETER
-    ---------
-    dev_code: str
-        e.g. TUMSTANDARDMODULE001
-    """
-
-    # get the list of month from start_time to end_time
-    start_time = datetime.date(2020, 10, 1)
-    end_time = datetime.datetime.now()
-    start_time_list = pd.date_range(start_time, end_time, freq='MS')
-    end_time_list = pd.date_range(start_time + relativedelta(months=+1),
-                                  end_time + relativedelta(months=+1),
-                                  freq='MS') - np.timedelta64(1, 's')
-    date_list = zip(start_time_list, end_time_list)
-
-    for date_from, date_to in date_list:
-        download_files_of_interest(dev_code, date_from, date_to)
-
-
-def download_files_of_interest(dev_code, date_from, date_to):
-    """Download the files of interest of a module in between a time period. Excludes unwanted, i.e. onc log files.
-    PARAMETER
-    ---------
-    dev_code: str
-        e.g. TUMSTANDARDMODULE001
-    date_from: datetime
-    date_to: datetime
-    """
-    dev_path = os.path.join(strawb.Config.raw_data_dir,
-                            dev_code.lower())
-
-    # create ONCDownloader
-    downloader_i = strawb.ONCDownloader(showInfo=False,
-                                        outPath=os.path.join(dev_path, date_from.strftime("%Y_%m"))
-                                        )
-
-    # downloader_i.start(filters=filters, allPages=True)  # start the download in a thread (background)
-    filters = {'deviceCode': dev_code,
-               'dateFrom': date_from.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
-               'dateTo': date_to.strftime("%Y-%m-%dT%H:%M:%S.999Z"),
-               'extension': 'hdf5'}
-
-    for extension_i in ['hdf5', 'raw', 'png', 'hld']:
-        filters['extension'] = extension_i
-        downloader_i.download_file(filters=filters, allPages=True)  # start the download in a thread (background)
-
-    filters.pop('extension')
-
-    # Time Over Threshold Data
-    for data_product_i in ['PMTTOT', 'LIDARTOT', 'MTTOT']:
-        filters['dataProductCode'] = data_product_i
-        downloader_i.download_file(filters=filters, allPages=True)  # start the download in a thread (background)
+onc_downloader = strawb.ONCDownloader()
 
 
 def main():
-    # # Get the dev_codes
-    # get all possible dev_codes
-    dev_codes = set([i['dev_code'] for i in strawb.module_onc_id.values()])
-    # subtract not deployed dev_codes
-    dev_codes = dev_codes.difference(['TEST', 'UNITTEST', 'TUMSTANDARDMODULE002', 'TUMSTANDARDMODULE003'])
+    # get all possible files from the devices
+    result = onc_downloader.get_files_for_dev_code(strawb.dev_codes_deployed,
+                                                   date_from=datetime.date(2021, 8, 1),
+                                                   date_to=datetime.datetime.now())
 
-    for i in dev_codes:
-        download_module(i)
+    # convert the list to a DataFrame for easier modifications
+    pd_result = pd.DataFrame.from_dict(result['files'])
+
+    # convert the datetime columns accordingly
+    for key_i in ['archivedDate', 'modifyDate', 'dateFrom', 'dateTo']:
+        pd_result[key_i] = pd.to_datetime(pd_result[key_i])
+
+    # add a 'outPath' column, to specify the outPath per file
+    pd_result['outPath'] = strawb.Config.raw_data_dir + '/'
+    pd_result['outPath'] += pd_result['deviceCode'].str.lower() + '/' + pd_result['dateFrom'].dt.strftime('%Y_%m')
+
+    # mask by file size
+    mask = pd_result['fileSize'] < .75e9
+
+    # ## Select dataProducts
+    # dataProduct_all = []
+    # for i in pd_result['dataProductCode'].unique():
+    #     print(i)
+    #     dataProduct_all.extend(onc.getDataProducts({'dataProductCode': i}))
+    #
+    # dataProduct_select = [i for i in dataProduct_all if i['extension'] in ['hdf5', 'hld', 'raw', 'png']]
+    # dataProduct_select.extend(
+    #     [i for i in dataProduct_all if i['extension'] == 'txt' and i['dataProductName'] != 'Log File'])
+
+    filters_or_result = dict(files=pd_result[mask].to_dict(orient='records'))
+    onc_downloader.getDirectFiles(filters_or_result=filters_or_result)
 
 
 if __name__ == "__main__":
