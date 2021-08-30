e910337b3b61c88c00cdacc8e7d2e6acc42a6e0b
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-08-30 11:09:09 +0200

    Added: - resources/scripts - added onc_vm_downloader.py - added docs

--- resources/scripts/onc_vm_downloader.py
+++ resources/scripts/onc_vm_downloader.py
@@ -4,13 +4,13 @@
 # WHAT THIS SCRIPT DOES
 # This script downloads all files from the ONC server for the all dev_codes defined in `strawb.dev_codes_deployed`
 # It will download it with the following structure:
-#   strawb.Config.raw_data_dir
-#       strawb.Config.raw_data_dir/$dev_codes[0]$  # i.e. tumlidar001
-#           strawb.Config.raw_data_dir/$dev_codes[0]$/2020_10
+#   $strawb.Config.raw_data_dir$
+#       $strawb.Config.raw_data_dir$/$dev_codes[0]$  # i.e. tumlidar001
+#           $strawb.Config.raw_data_dir$/$dev_codes[0]$/2020_10
 #               # all files from October
-#           strawb.Config.raw_data_dir/$dev_codes[0]$/2020_11
+#           $strawb.Config.raw_data_dir$/$dev_codes[0]$/2020_11
 #           ...
-#       strawb.Config.raw_data_dir/$dev_codes[1]$
+#       $strawb.Config.raw_data_dir$/$dev_codes[1]$
 #       ...
 # In addition, it saves the metadata from the ONC server for all files under: `strawb.Config.pandas_file_sync_db`
 # This file can be imported with: `pandas.read_pickle(strawb.Config.pandas_file_sync_db)` again.
