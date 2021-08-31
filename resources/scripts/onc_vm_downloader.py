64ab573874fb00b554c11f1577a929ff4a23e034
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-08-31 17:47:43 +0200

    added `download_structured`

--- resources/scripts/onc_vm_downloader.py
+++ resources/scripts/onc_vm_downloader.py
@@ -4,67 +4,30 @@
 """ WHAT THIS SCRIPT DOES
 This script downloads all files from the ONC server for the all dev_codes defined in `strawb.dev_codes_deployed`
 It will download it with the following structure:
-  $strawb.Config.raw_data_dir$
-      $strawb.Config.raw_data_dir$/$dev_codes[0]$  # i.e. tumlidar001
-          $strawb.Config.raw_data_dir$/$dev_codes[0]$/2020_10
+  <strawb.Config.raw_data_dir>
+      <strawb.Config.raw_data_dir>/<dev_codes[0]>  # i.e. tumlidar001
+          <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_10
               # all files from October
-          $strawb.Config.raw_data_dir$/$dev_codes[0]$/2020_11
+          <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_11
           ...
-      $strawb.Config.raw_data_dir$/$dev_codes[1]$
+      <strawb.Config.raw_data_dir>/<dev_codes[1]>
       ...
 In addition, it saves the metadata from the ONC server for all files under: `strawb.Config.pandas_file_sync_db`
 This file can be imported with: `pandas.read_pickle(strawb.Config.pandas_file_sync_db)` again.
 """
 
 import datetime
-import pandas as pd
 import strawb
-
-onc_downloader = strawb.ONCDownloader()
 
 
 def main():
-    # get all possible files from the devices
-    result = onc_downloader.get_files_for_dev_codes(strawb.dev_codes_deployed,
-                                                    date_from=datetime.date(2020, 10, 1),
-                                                    date_to=datetime.datetime.now())
-
-    # convert the list to a DataFrame for easier modifications
-    pd_result = pd.DataFrame.from_dict(result['files'])
-
-    # convert the datetime columns accordingly
-    for key_i in ['archivedDate', 'modifyDate', 'dateFrom', 'dateTo']:
-        pd_result[key_i] = pd.to_datetime(pd_result[key_i])
-
-    # add a 'outPath' column, to specify the outPath per file
-    pd_result['outPath'] = strawb.Config.raw_data_dir + '/'
-    pd_result['outPath'] += pd_result['deviceCode'].str.lower() + '/' + pd_result['dateFrom'].dt.strftime('%Y_%m')
-
-    # mask by file size
-    mask = pd_result['fileSize'] < .75e9
-    print(f'Exclude {len(mask[~mask])} files')
-
-    # ## Select dataProducts
-    # dataProduct_all = []
-    # for i in pd_result['dataProductCode'].unique():
-    #     print(i)
-    #     dataProduct_all.extend(onc.getDataProducts({'dataProductCode': i}))
-    #
-    # dataProduct_select = [i for i in dataProduct_all if i['extension'] in ['hdf5', 'hld', 'raw', 'png']]
-    # dataProduct_select.extend(
-    #     [i for i in dataProduct_all if i['extension'] == 'txt' and i['dataProductName'] != 'Log File'])
-
-    # reduce it with the mask and the columns to 'filename', 'outPath'
-    pd_result_masked = pd_result[mask][['filename', 'outPath']]
-    filters_or_result = dict(files=pd_result_masked.to_dict(orient='records'))
-    onc_downloader.getDirectFiles(filters_or_result=filters_or_result)
-
-    # add column of which files are synced
-    pd_result['synced'] = mask
-
-    # rename 'outPath' to 'fullPath' and cal. the full path
-    pd_result = pd_result.rename(columns={"outPath": "fullPath"})
-    pd_result["fullPath"] += '/' + pd_result['filename']
+    onc_downloader = strawb.ONCDownloader()
+    pd_result = onc_downloader.download_structured(dev_codes=strawb.dev_codes_deployed,
+                                                   extensions=None,
+                                                   date_from='strawb_all',
+                                                   date_to=None,
+                                                   download=True,
+                                                   )
 
     # store information in a pandas-file
     pd_result.to_pickle(strawb.Config.pandas_file_sync_db, protocol=4)  # protocol=4 compatible with python>=3.4
--- src/strawb/onc_downloader/__init__.py
+++ src/strawb/onc_downloader/__init__.py
@@ -3,8 +3,10 @@ import datetime
 import os
 import threading
 
+import pandas
 from onc.onc import ONC  # pip install onc; https://pypi.org/project/onc/
 
+import strawb
 from strawb.config_parser.__init__ import Config
 
 
@@ -69,6 +71,10 @@ class ONCDownloader(ONC):
             e.g. datetime.datetime.now()
         print_stats: bool, optional
             if the function returns stats
+        Returns
+        -------
+        result: dict
+            A dict with at least the key 'files' which is a list of all available files. Each file is a separated dict.
         """
 
         # get the result dict with all available files
@@ -91,4 +97,121 @@ class ONCDownloader(ONC):
 
         if print_stats:
             print(f'In total: {len(result["files"])} files')
+
         return result
+
+    def download_structured(self, dev_codes=None, date_from=None, date_to=None,
+                            extensions=None,  # data_product_names=None,
+                            min_file_size=0, max_file_size=.75e9, download=True):
+        """
+        This function syncs files from the onc server for the specified dev_codes. The files are organized in different
+        directories by <Config.raw_data_dir>/<dev_code>/<year>_<month>/<file_name>.
+        Files can be filtered by the file size, extensions,
+        PARAMETERS
+        ----------
+        date_from: str, datetime, optional
+            Takes on of the following:
+            - None: takes the today-1day;
+            - str in isoformat: '2021-08-31T14:33:25.209715';
+            - str: 'strawb_all' to get data since STRAWb went online
+            - timedelta: date_from = now - timedelta
+            - a datetime
+        date_to: str, datetime, timedelta, optional
+            Takes on of the following:
+            - None: takes the today;
+            - str in isoformat: '2021-08-31T14:33:25.209715';
+            - timedelta: date_to = date_from + timedelta
+            - a datetime
+        download: bool, optional
+            weather or not to download the files. True (default) downloads the files, and
+            False just checks which pass the filter.
+        dev_codes: list, optional
+            List with dev_codes for which the files are downloaded. None (default), takes all deployed STRAWb dev_codes
+        extensions: list[str], optional
+            A list of extensions to download. If None, it takes all possible extensions for STRAWb,
+            i.e. ['hdf5', 'hld', 'raw', 'png', 'txt'].
+        :return:
+        """
+        # TODO: add extensions=None, data_product_names
+        if dev_codes is None:
+            dev_codes = strawb.dev_codes_deployed
+
+        if date_from is None:
+            date_from = datetime.datetime.now() - datetime.timedelta(days=1)
+        elif date_from == 'strawb_all':
+            date_from = datetime.date(2020, 10, 1)
+        elif isinstance(date_from, datetime.datetime):
+            date_from = date_from
+        elif isinstance(date_from, datetime.timedelta):
+            date_from = datetime.datetime.now() - date_from
+        else:
+            date_from = datetime.datetime.fromisoformat(date_from)
+
+        if date_to is None:
+            date_to = datetime.datetime.now()
+        elif isinstance(date_to, datetime.datetime):
+            date_to = date_to
+        elif isinstance(date_to, datetime.timedelta):
+            date_to = date_from + date_to
+        else:
+            date_to = datetime.datetime.fromisoformat(date_to)
+
+        # get all possible files from the devices
+        result = self.get_files_for_dev_codes(dev_codes, date_from=date_from, date_to=date_to)
+
+        # convert the list to a DataFrame for easier modifications
+        pd_result = pandas.DataFrame.from_dict(result['files'])
+
+        # convert the datetime columns accordingly
+        for key_i in ['archivedDate', 'modifyDate', 'dateFrom', 'dateTo']:
+            pd_result[key_i] = pandas.to_datetime(pd_result[key_i])
+
+        # add a 'outPath' column, to specify the outPath per file
+        pd_result['outPath'] = strawb.Config.raw_data_dir + '/'
+        pd_result['outPath'] += pd_result['deviceCode'].str.lower() + '/' + pd_result['dateFrom'].dt.strftime('%Y_%m')
+
+        # mask by file size
+        mask = min_file_size <= pd_result['fileSize']
+        mask &= pd_result['fileSize'] <= max_file_size
+
+        # # Select dataProducts
+        # get more information for the existing dataProductCodes
+        dataProduct_all = []
+        if extensions is not None:  # and data_product_names is not None
+            for i in pd_result['dataProductCode'].unique():
+                dataProduct_all.extend(self.getDataProducts({'dataProductCode': i}))
+
+        if extensions is None:
+            pass
+        else:
+            mask_extensions = mask.copy()*0
+            if isinstance(extensions, str):
+                extensions = [extensions]
+
+            for i in extensions:
+                mask_extensions += pd_result['filename'].str.endswith(i)
+            mask &= mask_extensions
+        # if data_product_names is None:
+        #     pass
+        # else:
+        #     dataProduct_all = []
+        #     for i in pd_result['dataProductCode'].unique():
+        #         dataProduct_all.extend(self.getDataProducts({'dataProductCode': i}))
+        #     dataProduct_select = [i for i in dataProduct_all if i['dataProductName'] in data_product_names]
+        #     mask &= pd_result['dataProduct'] == dataProduct_select
+
+        print(f'Exclude {len(mask[~mask])} files')
+        if download:
+            # reduce it with the mask and the columns to 'filename', 'outPath'
+            pd_result_masked = pd_result[mask][['filename', 'outPath']]
+            filters_or_result = dict(files=pd_result_masked.to_dict(orient='records'))
+            self.getDirectFiles(filters_or_result=filters_or_result)
+
+        # add column of which files are synced
+        pd_result['synced'] = mask
+
+        # rename 'outPath' to 'fullPath' and cal. the full path
+        pd_result = pd_result.rename(columns={"outPath": "fullPath"})
+        pd_result["fullPath"] += '/' + pd_result['filename']
+
+        return pd_result
