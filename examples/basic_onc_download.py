00d7a42d380f7ec95db363fd01fa22cc4c718233
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-09-02 11:05:58 +0200

    update example

--- examples/basic_onc_download.py
+++ examples/basic_onc_download.py
@@ -1,6 +1,6 @@
 # This examples shows who to download files from the ONC server
 from src.strawb.onc_downloader import ONCDownloader
-from strawb import dev_codes_deployed
+import src.strawb as strawb
 
 onc_downloader = ONCDownloader(showInfo=False)
 
@@ -10,13 +10,13 @@ onc_downloader = ONCDownloader(showInfo=False)
 #  2. sets the direcotory for each file ('outPath'),
 #  (3. if download=True, downloads the files)
 #  4. returns the result as a pandas.DataFrame
-pd_result = onc_downloader.download_structured(dev_codes=dev_codes_deployed[:2],
+pd_result = onc_downloader.download_structured(dev_codes=strawb.dev_codes_deployed[:2],  # only 2 dev's for less data
                                                extensions=None,
                                                date_from='2021-08-30T00:00:00.000',
                                                date_to='2021-08-30T01:00:00.000',
                                                download=False,
                                                )
 # select only 5 files
-pd_result_masked = pd_result[:5]
+pd_result_masked = pd_result[:5]  # select only 5 files for less data
 # download the files
 onc_downloader.getDirectFiles(filters_or_result=pd_result_masked)
