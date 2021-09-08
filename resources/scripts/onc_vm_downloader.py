d90dce70a988f4dcdc1a09b55a245082c3ec8073
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-09-08 10:29:07 +0200

    added docs to onc_vm_downloader.py

--- resources/scripts/onc_vm_downloader.py
+++ resources/scripts/onc_vm_downloader.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 # coding: utf-8
 
-""" WHAT THIS SCRIPT DOES
-This script downloads all files from the ONC server for the all dev_codes defined in `strawb.dev_codes_deployed`
-It will download it with the following structure:
-  <strawb.Config.raw_data_dir>
-      <strawb.Config.raw_data_dir>/<dev_codes[0]>  # i.e. tumlidar001
-          <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_10
-              # all files from October
-          <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_11
-          ...
-      <strawb.Config.raw_data_dir>/<dev_codes[1]>
-      ...
-In addition, it saves the metadata from the ONC server for all files under: `strawb.Config.pandas_file_sync_db`
-This file can be imported with: `pandas.read_pickle(strawb.Config.pandas_file_sync_db)` again.
+"""
+WHAT THIS SCRIPT DOES
+---------------------
+1. This script downloads all files from the ONC server
+2. it saves the metadata from the ONC server for all files under:
+   >>> import strawb.Config
+   >>> strawb.Config.pandas_file_sync_db
 """
 
 import datetime
@@ -21,6 +15,24 @@ import strawb
 
 
 def main():
+    """
+    This function downloads all files from the ONC server for the all dev_codes defined in `strawb.dev_codes_deployed`
+    It will download it with the following structure:
+      <strawb.Config.raw_data_dir>
+          <strawb.Config.raw_data_dir>/<dev_codes[0]>  # i.e. tumlidar001
+              <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_10
+                  # all files from October
+              <strawb.Config.raw_data_dir>/<dev_codes[0]>/2020_11
+              ...
+          <strawb.Config.raw_data_dir>/<dev_codes[1]>
+          ...
+
+    In addition, it saves the metadata from the ONC server for all files under: `strawb.Config.pandas_file_sync_db`
+    This file can be imported again with:
+    >>> import pandas, strawb.Config
+    >>> pandas.read_pickle(strawb.Config.pandas_file_sync_db)
+    """
+
     onc_downloader = strawb.ONCDownloader()
     pd_result = onc_downloader.download_structured(dev_codes=strawb.dev_codes_deployed,
                                                    extensions=None,
