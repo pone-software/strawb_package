0d0f1907e6be9180647af3c7142ac3ff5b2afc05
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-09-09 16:16:09 +0200

    updated camera test

--- test/test_camera.py
+++ test/test_camera.py
@@ -36,30 +36,30 @@ class TestCameraFileHandlerInit(TestCase):
         print(self.full_path)
 
 
-# class TestCameraFileHandler(TestCase):
-#     def setUp(self):
-#         file_name = 'TUMPMTSPECTROMETER002_20210501T190000.000Z-SDAQ-CAMERA.hdf5'
-#         self.cam_run = FileHandler(file_name)
-
-
 class TestPictureHandler(TestCase):
     def setUp(self):
-        file_name = 'TUMPMTSPECTROMETER002_20210510T190000.000Z-SDAQ-CAMERA.hdf5'
-        cam_run = FileHandler(file_name)
+        file_list = FileHandler.find_files('*-SDAQ-CAMERA.hdf5',
+                                           directory=Config.raw_data_dir,
+                                           recursive=True,
+                                           raise_nothing_found=True)
+
+        self.full_path = random.choice(file_list)  # select a random file
+
+        cam_run = FileHandler(self.full_path)
         self.picture_handler = PictureHandler(cam_run)
 
     def test_image2png_lucifer(self):
         self.picture_handler.image2png_lucifer()
 
     def test_image2png_all(self):
-        self.cam_run.image2png(exclude_invalid=False)
+        self.picture_handler.image2png(exclude_invalid=False)
 
     def test_images_most_charge(self):
-        mode_list, mask_list = self.cam_run.get_lucifer_mask()
+        mode_list, mask_list = self.picture_handler.get_lucifer_mask()
         mask_lucifer = np.any(mask_list, axis=0)
 
-        mask = (self.cam_run.integrated_minus_dark > 5e3) & self.cam_run.invalid_mask & ~mask_lucifer
-        index = np.argsort(self.cam_run.integrated_minus_dark)
+        mask = (self.picture_handler.integrated_minus_dark > 5e3) & self.picture_handler.invalid_mask & ~mask_lucifer
+        index = np.argsort(self.picture_handler.integrated_minus_dark)
         index = index[mask[index]]  # remove invalid items  & cam_module.invalid_mask
         index = index[::-1]  # revers the order
-        self.cam_run.image2png(index=index, f_name_formatter='{i}_{datetime}.png')
+        self.picture_handler.image2png(index=index, f_name_formatter='{i}_{datetime}.png')
