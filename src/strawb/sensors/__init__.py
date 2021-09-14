f04e9d8245a793d0c4b2e42e3853655b068b95ac
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-09-14 14:35:43 +0200

    updated imports

--- src/strawb/sensors/__init__.py
+++ src/strawb/sensors/__init__.py
@@ -1,3 +1,4 @@
 from .camera import Camera
 from .module import Module
+from .lidar import Lidar
 
--- src/strawb/sensors/lidar/__init__.py
+++ src/strawb/sensors/lidar/__init__.py
@@ -1,5 +1,5 @@
 from .file_handler import FileHandler
 
 
-class Module:
+class Lidar:
     FileHandler = FileHandler
