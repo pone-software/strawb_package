324fcfaed4d31086f1d91965f71a07d3c3063ac6
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-10-12 14:26:22 +0200

    first commit of the LiDAR class

--- src/strawb/sensors/lidar/__init__.py
+++ src/strawb/sensors/lidar/__init__.py
@@ -1,5 +1,19 @@
+from ctypes import Union
+
+from .LidarProcessedDataStore import LidarProcessedDataStore
 from .file_handler import FileHandler
+from ...tools import TRBTools
 
 
-class Lidar:
-    FileHandler = FileHandler
+class Lidar(TRBTools):
+    # FileHandler = FileHandler
+
+    def __init__(self, name='', file: Union[str, FileHandler] = None):
+        self.name = name
+
+        if isinstance(file, str):
+            self.file_handler = FileHandler(file_name=file)
+        else:
+            self.file_handler = file
+
+        self.pds = LidarProcessedDataStore(file_handler=self.file_handler)
