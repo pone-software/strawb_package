6652a612c3c1be162d1b60c3e88c70f721410a0b
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-10-12 14:45:55 +0200

    added dataframe for rate

--- src/strawb/sensors/lidar/LidarProcessedDataStore.py
+++ src/strawb/sensors/lidar/LidarProcessedDataStore.py
@@ -1,3 +1,5 @@
+import pandas
+
 from strawb.base_processed_data_store import BaseProcessedDataStore
 from strawb.sensors.lidar.file_handler import FileHandler
 from strawb.tools import TRBTools
@@ -31,13 +33,23 @@ class LidarProcessedDataStore(BaseProcessedDataStore, TRBTools):
             self.calculate_counts()
         return self._rate_laser
 
-    def calculate_counts(self):
-        self._rate_time, rate_data = TRBTools._calculate_counts_(
-            daq_pulser_readout=self.file_handler.daq_pulser_readout,
-            ch_0=self.file_handler.counts_ch0,
-            ch_17=self.file_handler.counts_ch17,
-            ch_18=self.file_handler.counts_ch18)
+    def get_pandas_rate(self):
+        if self.file_handler.file_version >= 2:
+            return pandas.DataFrame(dict(time=self.file_handler.counts_time.asdatetime()[:],
+                                         rate_time=self.rate_time,
+                                         rate_pmt=self.rate_pmt,
+                                         rate_laser=self.rate_laser)
+                                    )
 
-        self._rate_pmt = rate_data[0]
-        self._rate_laser = rate_data[1]
-        return self._rate_time, rate_data
+    def calculate_counts(self):
+        if self.file_handler.file_version >= 2:
+            self._rate_time, rate_data = TRBTools._calculate_counts_(
+                daq_pulser_readout=self.file_handler.daq_pulser_readout,
+                ch_0=self.file_handler.counts_ch0,
+                ch_17=self.file_handler.counts_ch17,
+                ch_18=self.file_handler.counts_ch18)
+
+            self._rate_pmt = rate_data[0]
+            self._rate_laser = rate_data[1]
+            return self._rate_time, rate_data
+        return None
