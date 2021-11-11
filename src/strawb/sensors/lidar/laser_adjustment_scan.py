0a4a5d21e7de66b1d1f622d397bc8dd379b87d0e
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-11-11 12:38:37 +0100

    added TODOs and introduced properties

--- src/strawb/sensors/lidar/laser_adjustment_scan.py
+++ src/strawb/sensors/lidar/laser_adjustment_scan.py
@@ -3,26 +3,61 @@ from scipy.stats import binned_statistic
 import strawb.sensors.lidar
 
 
-class Analysis:
-    def __init__(self, file, lever_long=0.051, lever_short=0.038, rpm=31, delta_t_step=0.5, thread_steepness=0.002,
+class LaserAdjustmentScan:
+    def __init__(self, file, lever_long=0.051, lever_short=0.038, rpm=31., delta_t_step=0.5, thread_steepness=0.002,
                  efficiency=0.5):
+        """
+        TODO: add doc-string
+        PARAMETER
+        ---------
+        file
+        lever_long
+        lever_short
+        rpm: float, optional
+            [1/min] rotations per minute
+        delta_t_step
+        thread_steepness
+        efficiency
+        """
+        # TODO: add docs for parameters
+        self.file = file
         self.steps = None
         self.steps_length = None
-        self.file = file
         self.step_positions = None
 
-        self.rpm = rpm  # [1/min] rotations per minute
-        self.rps = self.rpm / 60.  # [1/s] rotations per second
+        self.rps = rpm / 60.  # [1/s] rotations per second
         self.delta_t_step = delta_t_step  # [s] seconds the motor moves per step
         self.thread_steepness = thread_steepness  # [m], thread steepness (distance per rotation)
         self.efficiency = efficiency  # the motor moves slow with friction, e.g. 50% slower -> .5
-        self.dx = self.rps * self.delta_t_step * self.thread_steepness * self.efficiency  # [m] distance per rotation
+
         self.x_1 = np.array([[lever_short, 0, 0]])  # [m]; x Position stepper
         self.x_2 = np.array([[0, lever_long, 0]])  # [m]; y Position stepper
         self.v_motor = np.array([[0, 0, self.dx]])  # the vector per motor-step
+
+        # TODO: add docs for parameters
         self.phi_2d = None
         self.theta_2d = None
         self.pmt_counts_2d = None
+
+    @property
+    def dx(self):
+        """Distance in meter [m] per rotation."""
+        return self.rps * self.delta_t_step * self.thread_steepness * self.efficiency  # [m] distance per rotation
+
+    @property
+    def rpm(self):
+        """The rotations per minute of the motor."""
+        return self.rps * 60.  # [1/min] rotations per min
+
+    @rpm.setter
+    def rpm(self, value):
+        """Sets the rotations per minute of the motor.
+        PARAMETER
+        ---------
+        value: float
+            rotations per minute in unit [1/m]
+        """
+        self.rps = value / 60.  # [1/m] -> [1/s] rotations per second
 
     def get_steps(self):
         """
@@ -32,8 +67,15 @@ class Analysis:
         :return:
         None
         """
+        # TODO: add docs for the whole function
         lidar = strawb.sensors.lidar.Lidar(file=self.file)
-        self.steps = lidar.file_handler.file_attributes["mes_steps"]
+
+        # "mes_steps" might be renamed to "measurement_steps" for coherent naming in newer versions, support both here
+        try:
+            self.steps = lidar.file_handler.file_attributes["mes_steps"]
+        except KeyError:
+            self.steps = lidar.file_handler.file_attributes["measurement_steps"]
+
         self.steps_length = int(2 * self.steps + 1)
 
         laser_steps_x = np.array(lidar.file_handler.laser_set_adjust_x)
@@ -65,6 +107,7 @@ class Analysis:
         Returns the correct mapping of the calculated values to the spiral and reshapes it into 2d array
 
         """
+        # TODO: add docs for the whole function
         lidar = strawb.sensors.lidar.Lidar(file=self.file)
 
         abs_timestamp_middle = (lidar.file_handler.counts_time[:-1] + lidar.file_handler.counts_time[1:]) * 0.5
@@ -107,10 +150,11 @@ class Analysis:
     def get_angles(self):
         """
         Calculates the steps that are taken in the directions s_0,s_1 via meshgrid. Identical to spiral but as the
-        values for a 2D grid. the angle of rotations are
-        n1 = [X_1, 0, V_motor * s_1]
-        n2 = [0, X_2, V_motor * s_1]
-        and the laser direction is the crossproduct of which theta and phi is calculated
+        values for a 2D grid. The laser points in the direction to the normal of the plan defined by the two vectors:
+        n1 = [X_1, 0, V_motor * s_1] and n2 = [0, X_2, V_motor * s_1]. The crossproduct of both give the normal the
+        direction of the laser of which theta and phi is calculated.
+
+        # TODO: update parameters
         PARAMETER
         ---------
         steps: int
@@ -118,8 +162,8 @@ class Analysis:
 
         RETURN
         ------
-        phi: array,float, len= (2*steps+1)**2
-        theta: array,float, len= (2*steps+1)**2
+        phi: ndarray(dtype=float, ndim=1, len=(2*steps+1)**2)
+        theta: ndarray(dtype=float, ndim=1, len=(2*steps+1)**2)
         """
         # ---- Steps ----
         # get the steps grid, e.g. steps in x and y from [-5,...,5]
@@ -155,6 +199,7 @@ class Analysis:
         s1: float, 1. coordinate for steps to the max value
         s2: float, 2. coordinate for steps to the max value
         """
+        # TODO: add docs for the whole function
         self.laser_adjustment_return()
         self.get_angles()
 
