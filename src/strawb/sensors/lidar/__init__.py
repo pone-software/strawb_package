eb3e011cee164c9019e2ee224f8b1adc2e3da43e
Kilian Holzapfel <kilian.holzapfel@tum.de>
2021-11-18 16:42:38 +0100

    updated import, arranged parameters

--- src/strawb/sensors/lidar/__init__.py
+++ src/strawb/sensors/lidar/__init__.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from strawb.sensors.lidar.laser_adjustment_scan_v2 import LaserAdjustmentScan
 from strawb.sensors.lidar.lidar_trb_rates import LidarTRBRates
 from strawb.sensors.lidar.file_handler import FileHandler
 
@@ -7,7 +8,7 @@ from strawb.sensors.lidar.file_handler import FileHandler
 class Lidar:
     # FileHandler = FileHandler
 
-    def __init__(self, name='', file: Union[str, FileHandler] = None):
+    def __init__(self, file: Union[str, FileHandler] = None, name=''):
         self.name = name
 
         if isinstance(file, str):
