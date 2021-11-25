41588b5f76ecfc83097719cb270c7ffed9e6df7a
Lisa Schumacher <lj.schumacher@tum.de>
2021-11-25 15:54:47 +0100

    Update tools.py
    added wavelength to rgb converter

--- src/strawb/tools.py
+++ src/strawb/tools.py
@@ -261,3 +261,57 @@ def human_size(size_bytes, units=None, precision=2):
     if units is None:
         units = [' bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']
     return f'{size_bytes:.{precision}f} {units[0]}' if size_bytes < 1024 else human_size(size_bytes / 1024, units[1:])
+
+
+def wavelength_to_rgb(channel, gamma=0.8):
+
+    '''This converts a given wavelength of light to an 
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+
+    try:
+        wavelength = float(channel["wavelength"])
+        alt_color = channel["color"]
+    except TypeError:
+        wavelength = channel
+        alt_color = "red"
+        
+    if wavelength >= 380 and wavelength <= 440:
+        attenuation = 0.3 + 0.7 * (wavelength - 380) / (440 - 380)
+        R = ((-(wavelength - 440) / (440 - 380)) * attenuation) ** gamma
+        G = 0.0
+        B = (1.0 * attenuation) ** gamma
+        return_this = (R, G, B)
+    elif wavelength >= 440 and wavelength <= 490:
+        R = 0.0
+        G = ((wavelength - 440) / (490 - 440)) ** gamma
+        B = 1.0
+        return_this = (R, G, B)
+    elif wavelength >= 490 and wavelength <= 510:
+        R = 0.0
+        G = 1.0
+        B = (-(wavelength - 510) / (510 - 490)) ** gamma
+        return_this = (R, G, B)
+    elif wavelength >= 510 and wavelength <= 580:
+        R = ((wavelength - 510) / (580 - 510)) ** gamma
+        G = 1.0
+        B = 0.0
+        return_this = (R, G, B)
+    elif wavelength >= 580 and wavelength <= 645:
+        R = 1.0
+        G = (-(wavelength - 645) / (645 - 580)) ** gamma
+        B = 0.0
+        return_this = (R, G, B)
+    elif wavelength >= 645 and wavelength <= 750:
+        attenuation = 0.3 + 0.7 * (750 - wavelength) / (750 - 645)
+        R = (1.0 * attenuation) ** gamma
+        G = 0.0
+        B = 0.0
+        return_this = (R, G, B)
+    else:
+        return_this = alt_color
+    return return_this
