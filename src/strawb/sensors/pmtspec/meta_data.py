c60044d3b3b2b9c88be51f5d46598884c09887d3
Kilian Holzapfel <kilian.holzapfel@tum.de>
2022-03-18 15:18:36 +0100

    improvements with the colors

--- src/strawb/sensors/pmtspec/meta_data.py
+++ src/strawb/sensors/pmtspec/meta_data.py
@@ -1,25 +1,27 @@
 # Author: Kilian Holzapfel <kilian.holzapfel@tum.de>
-
+import matplotlib.cm
 import numpy as np
 
 
 class PMTMetaData:
     """ Holds the MetaData for the PMTSpectrometer001 (the one which is working in the Pacific)"""
+
     def __init__(self):
         # The entries are:        [index, channel_name, wavelength, label]
-        dtype = [('index', int), ('channel', int), ('wavelength', int), ('label', object), ('color', object)]
-        channel_meta_array = np.array([(0, 1, 450, '', None),
-                                       (1, 3, 480, '', None),
-                                       (2, 5, 470, '', None),
-                                       (3, 6, 0, '', None),
-                                       (4, 7, 460, '', None),
-                                       (5, 8, 550, '', None),
-                                       (6, 9, 492, '', None),
-                                       (7, 10, 525, '', None),
-                                       (8, 11, 425, '', None),
-                                       (9, 12, 510, '', None),
-                                       (10, 13, 400, '', None),
-                                       (11, 15, 350, '', None)],
+        dtype = [('index', int), ('trb', int), ('dac', int), ('padiwa', int), ('wavelength', int),
+                 ('label', object), ('color', object)]
+        channel_meta_array = np.array([(0, 9, 3, 4, 450, '', None),  # or (0, 1, 3, 4, 450, '', None) ? 'channel'=1
+                                       (1, 3, 1, 7, 480, '', None),
+                                       (2, 5, 3, 6, 470, '', None),
+                                       (3, 6, 7, 14, 0, '', None),
+                                       (4, 7, 4, 5, 460, '', None),
+                                       (5, 8, 5, 13, 550, '', None),
+                                       (6, 1, 4, 8, 492, '', None),  # or (6, 9, 4, 8, 492, '', None) ? 'channel'=9
+                                       (7, 10, 6, 12, 525, '', None),
+                                       (8, 11, 2, 3, 425, '', None),
+                                       (9, 12, 5, 11, 510, '', None),
+                                       (10, 13, 1, 2, 400, '', None),
+                                       (11, 15, 0, 1, 350, '', None)],
                                       dtype=dtype)
 
         # sort in place
@@ -32,24 +34,40 @@ class PMTMetaData:
         # store in place
         self.channel_meta_array = channel_meta_array
 
-    def add_colors(self, color_map, no_filter_color='gray', invisible=None):
+        self.add_colors()  # color with default settings
+
+    def add_colors(self, color_map=None, no_filter_color='gray', invisible_cmap=None):
         """ Create colors.
         PARAMETERS
         ----------
-        color_map: matplotlib.cm
-            a matplotlib colormap, i.e. `color_map = matplotlib.cm.get_cmap('viridis')`
+        color_map: matplotlib.cm or str, optional
+            a matplotlib colormap, default (None) is `color_map = matplotlib.cm.get_cmap('viridis')`
+            If a string is provided its: color_map = matplotlib.cm.get_cmap(color_map)
         no_filter_color: matplotlib color or RGB(A)-Tuple, optional
             defines the color of the channel without filter.
+        invisible_cmap: matplotlib.cm, optional
+            the colors for invisible
         """
-        entries = self.channel_meta_array.shape[0]
-        self.channel_meta_array['color'] = [color_map(i / entries) for i in range(entries)]
-        
-        ## not-visible wavelengths
-        if invisible is not None:
+        if color_map is None:
+            color_map = matplotlib.cm.get_cmap('viridis')
+        elif isinstance(color_map, str):
+            color_map = matplotlib.cm.get_cmap(color_map)
+
+        self.channel_meta_array['color'] = None  # reset all colors to None
+
+        # not-visible wavelengths
+        if invisible_cmap is not None:
             m = self.channel_meta_array['wavelength'] < 380
             n_inv = np.count_nonzero(m)
-            for i in np.arange(len(m))[m]:
-                self.channel_meta_array['color'][i] = invisible(i / n_inv)
-        
+            for i in np.argwhere(m).flatten():
+                self.channel_meta_array['color'][i] = invisible_cmap(i / n_inv)
+
         if no_filter_color is not None:
             self.channel_meta_array['color'][self.channel_meta_array['wavelength'] == 0] = no_filter_color
+
+        # set missing (None) colors
+        # noinspection PyComparisonWithNone
+        m = self.channel_meta_array['color'] == None
+        n_inv = np.count_nonzero(m)
+        for i in np.argwhere(m).flatten():
+            self.channel_meta_array['color'][i] = color_map(i / n_inv)
