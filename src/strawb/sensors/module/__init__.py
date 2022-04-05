1e97dbb29417a6d0eebd533fa76e7980ad95eb52
Kilian Holzapfel <kilian.holzapfel@tum.de>
2022-04-05 23:54:02 +0200

    added Power

--- src/strawb/sensors/module/__init__.py
+++ src/strawb/sensors/module/__init__.py
@@ -1,15 +1,29 @@
 from typing import Union
 
 from .file_handler import FileHandler
+from .power import Power
 
 
 class Module:
     # FileHandler = FileHandler
 
     def __init__(self, file: Union[str, FileHandler] = None, name=''):
+        """The Module class.
+        PARAMETER
+        ---------
+        file: Union[str, FileHandler], optional
+            The filename as string or a instance of the module.FileHandler
+        name: str, optional
+            to give the class a name
+        """
+
         self.name = name
 
         if isinstance(file, str):
             self.file_handler = FileHandler(file_name=file)
         else:
             self.file_handler = file
+
+        self.power = Power(self.file_handler)
+
+
