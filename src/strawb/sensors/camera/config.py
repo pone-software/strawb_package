343c4a3de2aada8aa17ea1e6a57bdbf5143a547f
ga87coh@mytum.de <ga87coh@mytum.de>
2022-08-05 17:50:46 +0200

    update config

--- src/strawb/sensors/camera/config.py
+++ src/strawb/sensors/camera/config.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 class Config:
     def __init__(self) -> None:
-        self.mask_mounting = np.load('TUMPMTSPECTROMETER001_cam_mask.npy')
+        with np.load('mounting_mask.npz') as f:
+            self.mask_mounting = f['TUMPMTSPECTROMETER001']
         
-        with pd.read_csv('LEDs.csv') as led:
-            self.module_x = np.sum(led.center_of_mass_x[:4])/4  # x coordinate of the module above the camera
-            self.module_y = np.sum(led.center_of_mass_y[:4])/4  # y coordinate of the module above the camera
+        self.module_x = np.sum(pd.read_csv('LEDs.csv').center_of_mass_x[:4])/4  # x coordinate of the module above the camera
+        self.module_y = np.sum(pd.read_csv('LEDs.csv').center_of_mass_y[:4])/4  # y coordinate of the module above the camera
 
-            self.data_cable_x = [625, self.module_x]  # x coordinates of the start and end point of the data cable
-            self.data_cable_y = [300, self.module_y]  # y coordinates of the start and end point of the data cable
+        self.data_cable_x = [625, self.module_x]  # x coordinates of the start and end point of the data cable
+        self.data_cable_y = [300, self.module_y]  # y coordinates of the start and end point of the data cable
 
-            self.steel_cable_x = [678, self.module_x]  # x coordinates of the start and end point of the steel cable
-            self.steel_cable_y = [320, self.module_y]  # y coordinates of the start and end point of the steel cable
+        self.steel_cable_x = [678, self.module_x]  # x coordinates of the start and end point of the steel cable
+        self.steel_cable_y = [320, self.module_y]  # y coordinates of the start and end point of the steel cable
