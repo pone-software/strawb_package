098326792287db12e66366f99767fdd3012bebc5
Kilian Holzapfel <kilian.holzapfel@tum.de>
2022-12-23 17:27:20 +0100

    LiDAR: bug fix

--- src/strawb/sensors/lidar/file_handler.py
+++ src/strawb/sensors/lidar/file_handler.py
@@ -68,8 +68,8 @@ class FileHandler(BaseFileHandler):
     def __load_meta_data__(self, ):
         # order is important, tries to load newest first and oldest latest.
         err_list = []
-        for i in [self.__load_meta_data_v5__, self.__load_meta_data_v4__, self.__load_meta_data_v3__,
-                  self.__load_meta_data_v2__, self.__load_meta_data_v1__]:
+        for i in [self.__load_meta_data_v6__, self.__load_meta_data_v5__, self.__load_meta_data_v4__,
+                  self.__load_meta_data_v3__, self.__load_meta_data_v2__, self.__load_meta_data_v1__]:
             try:
                 i()  # try file versions
                 return
